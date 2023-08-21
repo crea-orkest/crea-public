dd32185b8f0e3b2f82d4a5db1e9a07739894479d
thijsvandiessen <thijsvandiessen@gmail.com>
2023-08-21 08:56:47 +0200

    remove apollo client for client side queries

--- src/graphql/gqlClient.ts
+++ src/graphql/gqlClient.ts
@@ -1,26 +1,12 @@
+import { HttpLink } from "@apollo/client/core";
 import { cacheConfig } from "./cacheConfig";
 import { linkConfig } from "./linkConfig";
 import { registerApolloClient } from "@apollo/experimental-nextjs-app-support/rsc";
 
 import {
-  ApolloClient,
-  HttpLink,
-  InMemoryCache,
-  createHttpLink,
-} from "@apollo/client/core";
-
-import {
   NextSSRApolloClient,
   NextSSRInMemoryCache,
 } from "@apollo/experimental-nextjs-app-support/ssr";
-
-export const client = new ApolloClient({
-  ssrMode: true,
-  cache: new InMemoryCache(cacheConfig),
-  link: createHttpLink(linkConfig),
-  name: "web",
-  version: "1.0",
-});
 
 export const { getClient } = registerApolloClient(() => {
   return new NextSSRApolloClient({
