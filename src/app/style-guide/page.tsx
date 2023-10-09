bb24a8f3df6e6c668414772b08c1440107546925
Thijs <thijsvandiessen@gmail.com>
2023-10-09 11:16:22 +0200

    feat: style-guide-page

---
+++ src/app/style-guide/page.tsx
@@ -0,0 +1,26 @@
+const Page = () => {
+  return (
+    <main>
+      <h1>style guide</h1>
+
+      <h2>headings</h2>
+      <h3>h3 heading</h3>
+      <h4>h4 heading</h4>
+
+      <hr />
+
+      <h2>Paragraph</h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+        veniam, quis nostrud <strong>exercitation</strong> ullamco laboris nisi
+        ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+        reprehenderit in <em>voluptate</em> velit esse cillum dolore eu fugiat
+        nulla pariatur. Excepteur sint occaecat <mark>cupidatat</mark> non
+        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </p>
+    </main>
+  )
+}
+
+export default Page
