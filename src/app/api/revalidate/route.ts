7a0451c26c2e1191e5b25d51b21ccb1de770bbe9
Thijs <thijsvandiessen@gmail.com>
2023-10-12 09:51:16 +0200

    feat: revalidate datocms

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,18 +1,45 @@
 import { type NextRequest } from 'next/server'
 import { revalidateTag } from 'next/cache'
 
+const CMS_WEBHOOK_URL = 'https://webhooks.datocms.com/Vt1R8r6BD5/deploy-results'
+
 // e.g a webhook to `your-website.com/api/revalidate?tag=content&secret=super-secret
 export function POST(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get('secret')
   const tag = request.nextUrl.searchParams.get('tag')
 
   if (secret !== process.env.SECRET_TOKEN) {
-    return Response.json({ message: 'Invalid secret' }, { status: 401 })
+    const status = 'error'
+    fetch(CMS_WEBHOOK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.parse(status),
+    })
+    return Response.json({ status }, { status: 401 })
   }
 
   if (!tag) {
-    return Response.json({ message: 'Missing tag param' }, { status: 400 })
+    const status = 'Missing tag param'
+    fetch(CMS_WEBHOOK_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.parse(status),
+    })
+    return Response.json({ status }, { status: 400 })
   }
+
+  const status = 'success'
+  fetch(CMS_WEBHOOK_URL, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.parse(status),
+  })
 
   revalidateTag(tag)
 
