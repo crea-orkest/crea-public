d196b7d9019b35818f8ecbcb5bfefd691c91319f
velomovies <22169955+velomovies@users.noreply.github.com>
2023-11-01 17:52:03 +0100

    feat(header): use header styling and make sure image renders correctly

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import type { ElementType } from 'react'
 import classNames from 'classnames'
 import styles from './styles.module.scss'
 import { type HeaderFragment } from 'graphql/generated/graphql'
@@ -6,21 +7,39 @@ import { ContentField } from 'components/contentField'
 import Image from 'next/image'
 
 interface Props {
+  className?: string
+  tag?: ElementType
   data: HeaderFragment
 }
 
-export const Header: React.FC<Props> = ({ data }: Props) => {
+export const Header: React.FC<Props> = ({
+  className,
+  tag = 'header',
+  data,
+}: Props) => {
+  const HeaderTag = tag
+
   return (
-    <header className={classNames(styles.header)}>
-      <ContentField data={data.body} />
+    <HeaderTag
+      className={classNames(className, styles.header, 'with-background', {
+        [`${styles.withImage}`]: data.cover?.item?.url,
+      })}
+    >
+      <div className={classNames(styles.headerContent, 'content-layout')}>
+        <ContentField data={data.body} />
+      </div>
       {data.cover?.item?.url && (
-        <Image
-          alt={data.cover.item?.alt || data.cover.item?.title || ''}
-          src={data.cover?.item?.url}
-          width={100}
-          height={100}
-        />
+        <div className={classNames(styles.headerImageWrapper)}>
+          <Image
+            className={classNames(styles.headerImage)}
+            alt={data.cover.item?.alt || data.cover.item?.title || ''}
+            src={data.cover?.item?.url}
+            width={data.cover?.item.width || 100}
+            height={data.cover?.item.height || 100}
+          />
+          <div className={classNames(styles.background)} />
+        </div>
       )}
-    </header>
+    </HeaderTag>
   )
 }
