a0b879f43ac53bd27ad67a304f1d5821f060ef0e
Thijs <thijsvandiessen@gmail.com>
2023-11-09 16:20:22 +0100

    feat: svg react icons

---
+++ src/components/icons/cross.tsx
@@ -0,0 +1,14 @@
+export const Cross = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="50"
+    height="40"
+    viewBox="0 0 50 50"
+    stroke="black"
+    strokeWidth="5"
+    strokeLinecap="round"
+  >
+    <line x2="50" y2="50" />
+    <line x1="50" y2="50" />
+  </svg>
+)
---
+++ src/components/icons/hamburger.tsx
@@ -0,0 +1,12 @@
+export const Hamburger = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 100 80"
+    width="50"
+    height="40"
+  >
+    <rect y="0" width="100" height="6"></rect>
+    <rect y="37" width="100" height="6"></rect>
+    <rect y="74" width="100" height="6"></rect>
+  </svg>
+)
