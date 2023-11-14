ec2aff6512aa9d22b9cfa9c9b387f43cf47181b5
Thijs <thijsvandiessen@gmail.com>
2023-11-14 12:55:02 +0100

    fix: 404 text

--- src/components/notFound/notFound.tsx
+++ src/components/notFound/notFound.tsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Link from 'next/link'
+import styles from './styles.module.scss'
 
 export const NotFound: React.FC = () => {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
-    </div>
+    <section className={styles.notFound}>
+      <div className={styles.notFound__content}>
+        <h2>Not Found</h2>
+        <p>Could not find requested resource</p>
+        <Link href="/">Return Home</Link>
+      </div>
+    </section>
   )
 }
