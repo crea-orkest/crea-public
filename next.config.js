db10bdd9384b75d9c98e90ff56b44a585f4dac29
Thijs <thijsvandiessen@gmail.com>
2023-11-14 11:35:51 +0100

    fix: cloudflare csp

--- next.config.js
+++ next.config.js
@@ -79,7 +79,7 @@ const nextConfig = {
         },
         {
           key: 'Content-Security-Policy',
-          value: `default-src 'unsafe-inline' 'self' https://main--crea-orkest-p7nmyh.apollographos.net; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com; manifest-src https://creaorkest.nl/manifest.json; img-src 'self' 'unsafe-inline' data: https://maps.gstatic.com https://maps.googleapis.com; connect-src 'self' https://maps.googleapis.com; script-src-elem https://maps.googleapis.com 'self' 'unsafe-inline'; style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;`,
+          value: `default-src 'unsafe-inline' 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com; manifest-src https://creaorkest.nl/manifest.json; img-src 'self' 'unsafe-inline' data: https://maps.gstatic.com https://maps.googleapis.com; connect-src 'self' https://maps.googleapis.com; script-src-elem https://static.cloudflareinsights.com https://maps.googleapis.com 'self' 'unsafe-inline'; style-src-elem 'self' 'unsafe-inline' https://fonts.googleapis.com; font-src 'self' https://fonts.gstatic.com;`,
         },
       ],
     },
