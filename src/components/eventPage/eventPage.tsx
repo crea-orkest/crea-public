ff4c97c677820e6ad2efa91e5e5e679602cc7323
velomovies <22169955+velomovies@users.noreply.github.com>
2023-11-29 16:50:21 +0100

    feat(eventPage): add image and add styling

--- src/components/eventPage/eventPage.tsx
+++ src/components/eventPage/eventPage.tsx
@@ -1,9 +1,10 @@
-import { PageContent } from 'components/pageContent'
-import { getEventPage } from '../../graphql/getters/getEventPage'
+import Image from 'next/image'
 import { notFound } from 'next/navigation'
+import classNames from 'classnames'
+import { PageContent } from 'components/pageContent'
 import { LocationDetail } from 'components/locationDetail'
-// import { EventListItem } from 'components/eventListItem'
-import { Header } from 'components/header'
+import { EventListItem } from 'components/eventListItem'
+import { getEventPage } from '../../graphql/getters/getEventPage'
 
 import styles from './styles.module.scss'
 
@@ -16,20 +17,53 @@ export const EventPage = async ({ slug }: Props) => {
   if (!data) return notFound()
 
   return (
-    <article>
-      <Header title={data.title} tag="header" />
-      {/* <div className={styles.calendar}>
-        <EventListItem
-          data={data}
-          size="large"
-          isLast={true}
-          title="Agenda"
-          showLink={false}
-          showImage={false}
-        />
-      </div> */}
+    <article className={styles.root}>
+      <header className={classNames(styles.header, 'with-background')}>
+        <div className={classNames(styles.titleContainer, 'content-layout')}>
+          <h2>{data.title}</h2>
+        </div>
+      </header>
+      <div
+        className={classNames(styles.container, 'content-layout', {
+          [`${styles.noGrid}`]: !data.image?.url,
+        })}
+      >
+        <aside className={styles.sidebar}>
+          <div className={styles.sidebarContent}>
+            {data.image?.url && (
+              <div className={classNames(styles.imageContainer)}>
+                <Image
+                  priority={true} // TODO: only for the first item
+                  className={classNames(styles.image)}
+                  src={data.image.url}
+                  alt={data.image.description}
+                  width={data.image.width ?? 100}
+                  height={data.image.height ?? 100}
+                />
+              </div>
+            )}
+          </div>
+        </aside>
+        <div className={styles.content}>
+          {data.locations.length > 0 && (
+            <div className={styles.eventListItem}>
+              <EventListItem
+                data={data}
+                size="large"
+                isLast={true}
+                title="Agenda"
+                showLink={false}
+                showImage={false}
+              />
+            </div>
+          )}
 
-      <PageContent items={data.content} />
+          <PageContent
+            sectionClassName={styles.pageContent}
+            items={data.content}
+          />
+        </div>
+      </div>
       <div className={styles.locations}>
         {data.locations.map((location) => {
           if (!location?.id || !location.startTime) return null
