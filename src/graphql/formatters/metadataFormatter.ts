de6d61f7fb545dd3a812f98d763d211169941bc0
velomovies <22169955+velomovies@users.noreply.github.com>
2024-02-07 20:29:38 +0100

    feat(sitemap.xml): implement indexPage

--- src/graphql/formatters/metadataFormatter.ts
+++ src/graphql/formatters/metadataFormatter.ts
@@ -21,44 +21,56 @@ export const metadataFormatter = (
   const defaultTitle = metaData.title
   const pageTitle = metaTitleFormatter(defaultTitle, data)
 
+  const indexPage = data?.__typename === 'PageRecord' &&
+    !data.indexPage && {
+      tag: 'meta',
+      attributes: {
+        name: 'robots',
+        content: 'noindex,nofollow',
+      },
+    }
+
+  const metaTags = [
+    {
+      tag: 'meta',
+      attributes: {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+    },
+    {
+      tag: 'meta',
+      attributes: {
+        rel: 'canonical',
+        href: slug === 'homepage' ? base : `${base}/${slug}`,
+      },
+    },
+    ...(data?._seoMetaTags || []),
+    ...(indexPage ? [indexPage] : []),
+    {
+      tag: 'title',
+      content: pageTitle,
+    },
+    {
+      tag: 'meta',
+      attributes: {
+        property: 'og:title',
+        content: pageTitle,
+      },
+    },
+    {
+      tag: 'meta',
+      attributes: {
+        name: 'twitter:title',
+        content: pageTitle,
+      },
+    },
+    // TODO add all icons
+  ]
+
   return {
     baseUrl: base,
     siteName: defaultTitle,
-    metaTags: [
-      {
-        tag: 'meta',
-        attributes: {
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1',
-        },
-      },
-      {
-        tag: 'meta',
-        attributes: {
-          rel: 'canonical',
-          href: slug === 'homepage' ? base : `${base}/${slug}`,
-        },
-      },
-      ...(data?._seoMetaTags || []),
-      {
-        tag: 'title',
-        content: pageTitle,
-      },
-      {
-        tag: 'meta',
-        attributes: {
-          property: 'og:title',
-          content: pageTitle,
-        },
-      },
-      {
-        tag: 'meta',
-        attributes: {
-          name: 'twitter:title',
-          content: pageTitle,
-        },
-      },
-      // TODO add all icons
-    ],
+    metaTags,
   }
 }
