c08839cf0e9bec4bc6679e437a1aac32a2152ed5
Thijs <thijsvandiessen@gmail.com>
2024-12-08 16:03:04 +0100

    fix: aws iam warnings

--- terraform/eventQueue.tf
+++ terraform/eventQueue.tf
@@ -10,13 +10,25 @@ data "aws_iam_policy_document" "instance_assume_role_policy" {
 }
 
 resource "aws_iam_role" "event_queue_iam_role" {
-  name = "event_queue_iam_role"
-
-  managed_policy_arns = ["arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"]
-  assume_role_policy  = data.aws_iam_policy_document.instance_assume_role_policy.json
-  inline_policy {}
+  name               = "event_queue_iam_role"
+  assume_role_policy = data.aws_iam_policy_document.instance_assume_role_policy.json
 
   tags = {
     tag-key = "crea-public"
   }
 }
+
+resource "aws_iam_role_policy_attachment" "event_queue_iam_role_policy_attachment" {
+  role       = aws_iam_role.event_queue_iam_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaSQSQueueExecutionRole"
+}
+
+resource "aws_iam_role_policy" "event_queue_iam_role_policy" {
+  name = "event_queue_iam_role_policy"
+  role = aws_iam_role.event_queue_iam_role.id
+  policy = jsonencode({
+    Version   = "2012-10-17"
+    Statement = []
+  })
+}
+
