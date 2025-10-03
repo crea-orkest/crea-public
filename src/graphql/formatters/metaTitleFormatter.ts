bb7b08a208f58cbb28d658020d808d2e9fc14c1a
Thijs <thijsvandiessen@users.noreply.github.com>
2025-10-03 21:02:24 +0200

    fix: prerender error missing title (#1514)
    fixes a build  issue

--- src/graphql/formatters/metaTitleFormatter.ts
+++ src/graphql/formatters/metaTitleFormatter.ts
@@ -9,7 +9,7 @@ export const metaTitleFormatter = (
   defaultTitle: string,
   data: PageDetailSeoFragment | EventPageSeoFragment | undefined | null
 ) => {
-  if (data?.seo?.title) return data.seo.title
+  if (typeof data?.seo?.title === 'string') return data.seo.title
   if (data?.title === 'Homepage') return defaultTitle
   if (data?.__typename === 'ConcertRecord' && typeof data?.title === 'string')
     return `${data.title} ${seperator} Concerten ${seperator} ${defaultTitle}`
