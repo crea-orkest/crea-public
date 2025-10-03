dcf16bd64fbc730245530751c1abbc8ff0604276
Thijs <thijsvandiessen@users.noreply.github.com>
2025-10-03 20:28:28 +0200

    fix: extra checks for undefined title (#1513)
    it is possible that at runtime the title is null or undefined

--- src/graphql/formatters/metaTitleFormatter.ts
+++ src/graphql/formatters/metaTitleFormatter.ts
@@ -7,11 +7,11 @@ const seperator = '|'
 
 export const metaTitleFormatter = (
   defaultTitle: string,
-  data: PageDetailSeoFragment | EventPageSeoFragment | undefined
+  data: PageDetailSeoFragment | EventPageSeoFragment | undefined | null
 ) => {
   if (data?.seo?.title) return data.seo.title
   if (data?.title === 'Homepage') return defaultTitle
-  if (data?.__typename === 'ConcertRecord')
+  if (data?.__typename === 'ConcertRecord' && typeof data?.title === 'string')
     return `${data.title} ${seperator} Concerten ${seperator} ${defaultTitle}`
   if (data?.title) return `${data.title} ${seperator} ${defaultTitle}`
 
