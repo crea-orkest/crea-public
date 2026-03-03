892bf5fe7f091d00a140e0cd460543dd3a4b9905
velomovies <22169955+velomovies@users.noreply.github.com>
2026-03-04 00:18:31 +0100

    fix: get token with function and error

--- src/graphql/linkConfig.ts
+++ src/graphql/linkConfig.ts
@@ -1,5 +1,17 @@
 const production: boolean =
-  process.env.NEXT_PUBLIC_PRODUCTION === 'true' ? true : true
+  process.env.NEXT_PUBLIC_PRODUCTION === 'true' ? true : false
+
+function getToken() {
+  const datoCmsReadOnlyToken = production
+    ? process.env['PROD_DATOCMS_READONLY_API_TOKEN']
+    : process.env['DEV_DATOCMS_READONLY_API_TOKEN']
+  if (!datoCmsReadOnlyToken) {
+    throw new Error(
+      'DATO_CMS_READ_ONLY_TOKEN environment variable should be set.'
+    )
+  }
+  return datoCmsReadOnlyToken
+}
 
 export const linkConfig = {
   url: 'https://graphql.datocms.com',
@@ -7,11 +19,7 @@ export const linkConfig = {
     'Content-Language': 'nl-NL',
     'Content-Type': 'application/json',
     Accept: 'application/json',
-    Authorization: `Bearer ${
-      production
-        ? process.env.PROD_DATOCMS_READONLY_API_TOKEN
-        : process.env.DEV_DATOCMS_READONLY_API_TOKEN
-    }`,
+    Authorization: `Bearer ${getToken()}`,
     'X-Environment': 'main',
     'X-Exclude-Invalid': 'true',
   },
